e4116cae7a456e82b659936330d3e84072e0d5f9
Auryn8 <deni.mor.ca@gmail.com>
2018-01-23 08:16:06 -0600

    Añade comentarios en readme

--- js/app.js
+++ js/app.js
@@ -1,36 +1,37 @@
  $(document).ready(function() {
- /* Vista Splash*/
+ /* Vista Splash
    setTimeout(function() {
     window.location.href = 'index02.html';
   }, 5000);
-})
+})*/
    
-   /*Función para redireccionar*/
+   /*Función para redireccionar a vista Index03*/
   $("#sign-up").click(function () {
-    window.location = '../views/index03.html'
+    window.location.replace = './index03.html'
   });
-  /*Función para habilitar el boton cuando se introduzcan 10 dígitos*/
-  $("#tel").keydown(function () {
-    var phone = $(this).val();
+  /*Función para habilitar el boton cuando se introduzcan 10 dígitos
+  $("#cel").keydown(function () {
+    var phone = $(this).content();
     for (var i = 0; i < 1; i++) {
-      if (phone.length == 8) {
-        $("#in-btn").removeAttr("disabled")
-      } else {
-        $("#in-btn").attr("disabled", "disabled")
-      };
+      if (phone.length == 10) {
+        $("#bt-on").removeAttr("disabled")
+      } 
     };
-  });
+  });*/
 
-  $("#in-btn").click(function () {
-    window.location = '../views/index04.html'
+   /*Redireccionar a vista Index04*/
+  $("#btn-on").click(function () {
+    window.location.replace = 'index04.html'
   });
-
-  $("#next-btn").click(function () {
-    window.location = '../views/index05.html'
+   
+  /*Redireccionar a vista Index05*/ 
+  $("#btn-next").click(function () {
+    window.location = 'index05.html'
   });
   
+   /*Redireccionar a vista Index06*/
   $("#to-btn").click(function () {
-    window.location = '../views/index06.html'
+    window.location = 'index06.html'
 
   });
 
